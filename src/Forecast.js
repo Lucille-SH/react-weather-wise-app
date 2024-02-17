a4f0506dd34feb73d5a57825dd165a1cda06135b
Lucille-SH <145914878+Lucille-SH@users.noreply.github.com>
2024-02-17 09:51:43 +0000

    Added useEffect

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ForecastDay from "./ForecastDay";
 
 import "./styles/Forecast.css";
@@ -8,31 +8,37 @@ export default function Forecast(props) {
   let [forecastData, setForecastData] = useState("");
   let [ready, setReady] = useState(false);
 
+  useEffect(() => {
+    setReady(false);
+  }, [props.coordinates]);
+
   function createForecastData(response) {
     setForecastData(response.data.daily);
     setReady(true);
   }
 
-  if (ready === true) {
+  if (ready) {
     return (
       <div className="grid Forecast">
-        {forecastData.map(function (DailyForecast, index) {
-          if (index < 5) {
-            return (
-              <div className="row weatherForecast" key={index}>
-                <ForecastDay data={DailyForecast} />
-              </div>
-            );
-          } else {
-            return null;
-          }
-        })}
+        <div className="row weatherForecast">
+          {forecastData.map(function (DailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="weatherForecastDay col-sm m-1" key={index}>
+                  <ForecastDay data={DailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
       </div>
     );
   } else {
     let apiKey = "2bd326a60dc89a53287e446e819664df";
-    let latitude = props.latitude;
-    let longitude = props.longitude;
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
     let forecastApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     axios.get(forecastApiUrl).then(createForecastData);
 
